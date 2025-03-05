ae502e9155f2b546b687b0dc39705689c22d1cbe
kiray96 <inesmartinsbarradas1996@gmail.com>
2025-03-05 22:49:55 +0000

    Create a program to replace a given character in a string with x

---
+++ ChangeString/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ChangeString
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string str;
+            char ch;
+
+            Console.WriteLine("Escreve uma string");
+            str = Console.ReadLine();
+
+            Console.WriteLine("Escreve um caráter");
+            ch = char.Parse(Console.ReadLine());
+
+            foreach(char letra in str)
+            {
+                if (letra == ch)
+                {
+                    Console.Write("x");
+                }
+                else
+                {
+                    Console.Write(letra);
+                }   
+            }
+
+
+        }
+    }
+}
