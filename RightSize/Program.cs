cecc2d68d558b936a6191ee7e2aac45f15d67e37
kiray96 <inesmartinsbarradas1996@gmail.com>
2025-03-05 22:08:43 +0000

    Add comments to the code

--- RightSize/Program.cs
+++ RightSize/Program.cs
@@ -8,14 +8,18 @@ namespace RightSize
         {
             foreach(string str in args)
             {
+                //If the string length is superior or equal to 8
                 if (str.Length >= 8)
                 {
+                    //Print the line requested string and break
                     Console.WriteLine("[EARLY STOP]");
                     break;
                 }
                 
+                //If the string length is superior to 3
                 if (str.Length > 3)
                 {
+                    //Print the string
                     Console.WriteLine(str);
                 }
                 
