848a0f6028fe6589d681830783a8787702d86f85
kiray96 <inesmartinsbarradas1996@gmail.com>
2025-03-05 22:03:31 +0000

    Create project to print arguments

---
+++ RightSize/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RightSize
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            foreach(string str in args)
+            {
+                if (str.Length >= 8)
+                {
+                    Console.WriteLine("[EARLY STOP]");
+                    break;
+                }
+                
+                if (str.Length > 3)
+                {
+                    Console.WriteLine(str);
+                }
+                
+            }
+        }
+    }
+}
