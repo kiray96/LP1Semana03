b7d21d72bb3e3196acccc47be3cdbd69f7abd9c5
kiray96 <inesmartinsbarradas1996@gmail.com>
2025-03-06 19:12:53 +0000

    Create project HeroPerk with perk enum

---
+++ HeroPerk/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HeroPerk
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Perks perks = 0;
+        }
+    }
+}
