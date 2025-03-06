da1b9b28dccd02b21b545c03beb9073bdd182e1e
kiray96 <inesmartinsbarradas1996@gmail.com>
2025-03-06 19:45:22 +0000

    Add parsing of perks input

--- HeroPerk/Program.cs
+++ HeroPerk/Program.cs
@@ -7,6 +7,29 @@ namespace HeroPerk
         private static void Main(string[] args)
         {
             Perks perks = 0;
+            String inputs = args[0];
+
+            foreach(char input in inputs)
+            {
+                if (input == 'w')
+                {
+                    perks ^= Perks.WarpShift;
+                }
+                else if (input == 'a')
+                {
+                    perks ^= Perks.AutoHeal;
+                }
+                else if (input == 's')
+                {
+                    perks ^= Perks.Stealth;
+                }
+                else if (input == 'd')
+                {
+                    perks ^= Perks.DoubleJump;
+                }
+            }
+
+            Console.WriteLine(perks);
         }
     }
 }
