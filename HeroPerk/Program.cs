b89c1f4abc5285d7d19d1f5bd78d2592a2d2d83e
kiray96 <inesmartinsbarradas1996@gmail.com>
2025-03-06 20:24:43 +0000

    Add comments to the code

--- HeroPerk/Program.cs
+++ HeroPerk/Program.cs
@@ -6,12 +6,20 @@ namespace HeroPerk
     {
         private static void Main(string[] args)
         {
+            // Definir a variavel onde os perks ficam guardados
             Perks perks = 0;
+
+            // Definir caso em que dois perks juntos significam silent jumper
             Perks silentJumper = Perks.Stealth | Perks.DoubleJump;
+
+            // Boleano para registar que foi encontrado input invalido 
             bool unknown = false;
+
+            // Obter input dos argumentos do programa
             String inputs = args[0];
 
-
+            // Percorrer todos os caracteres de input e ativar ou desativar os 
+            // respectivos perks
             foreach(char input in inputs)
             {
                 if (input == 'w')
@@ -30,6 +38,7 @@ namespace HeroPerk
                 {
                     perks ^= Perks.DoubleJump;
                 }
+                // Input desconhecido, quebrar o loop
                 else
                 {
                     unknown = true;
@@ -37,28 +46,33 @@ namespace HeroPerk
                 }
             }
 
-
+            // Print o caso unknown perk
             if (unknown)
             {
                 Console.WriteLine("!Unknown perk!");
             }
             else
             {
+                // Verifica se alguma perk está definido e imprime  "no perks" 
+                // caso não esteja ou imprime os perks existentes
                 if(perks == 0)
                 {
                     Console.WriteLine("!No perks at all!");
                 }
-
                 else
                 {
                     Console.WriteLine(perks);
                 } 
 
+                // Verifica se ambos os perks de silentJumper estão presentes e 
+                // impreme silentJumper
                 if ((perks & silentJumper) == silentJumper)
                 {
                     Console.WriteLine("!Silent jumper!");
                 }
 
+                // Verifica se autoheal nao está presente e imprime "not gonna
+                // make it" se for esse o caso
                 if ((perks & Perks.AutoHeal) != Perks.AutoHeal)
                 {
                     Console.WriteLine("!Not gonna make it!");
